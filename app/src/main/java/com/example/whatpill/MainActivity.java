da1fe427330f96e165051410e25375afc8eee360
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-14 18:38:06 +0900

    firebase 연동

--- app/src/main/java/com/example/whatpill/MainActivity.java
+++ app/src/main/java/com/example/whatpill/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.whatpill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView iv_camera;
+    private Button btn_camera;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -22,22 +27,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        iv_camera = (ImageView)findViewById(R.id.iv_camera);
+        btn_camera = (Button) findViewById(R.id.btn_camera);
 
-        iv_camera.setOnClickListener(new View.OnClickListener() {
+        btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
+            public void onClick(View v) {
+                if (isExistsCameraApplication()){
+                    Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraApp, 10000);
+                }
             }
         });
     }
 
+    private boolean isExistsCameraApplication() {
+        PackageManager packageManager = getPackageManager();
+
+        Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        List<ResolveInfo> cameraApps = packageManager.queryIntentActivities(cameraApp, PackageManager.MATCH_DEFAULT_ONLY);
+
+        return cameraApps.size() > 0;
+    }
+
     // camera intent 전달
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
         }
     }
 }
