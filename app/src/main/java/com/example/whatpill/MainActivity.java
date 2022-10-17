aea8ed9708cf973b4e2ef8cc39f8ef1f42bcaa2e
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-17 20:13:30 +0900

    dddd

--- app/src/main/java/com/example/whatpill/MainActivity.java
+++ app/src/main/java/com/example/whatpill/MainActivity.java
@@ -1,61 +1,177 @@
 package com.example.whatpill;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn_camera;
+    final static int TAKE_PICTURE = 1;
+    final static int GET_GALLERY_IMAGE =2;
+    private static final String TAG = "MyActivity";
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Button btnCamera, btnGallery, btnSave;
+    ImageView ivPill;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_camera = (Button) findViewById(R.id.btn_camera);
+        btnCamera = findViewById(R.id.btnCamera);
+        btnGallery = findViewById(R.id.btnGallery);
+        btnSave = findViewById(R.id.btnSave);
+        ivPill = findViewById(R.id.ivPill);
 
-        btn_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isExistsCameraApplication()){
-                    Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraApp, 10000);
-                }
+        btnCamera.setOnClickListener(this);
+        btnGallery.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+
+        // 권한 체크 후 권한 요청
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    ==checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)){}
+            else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},1 );
             }
-        });
-    }
-
-    private boolean isExistsCameraApplication() {
-        PackageManager packageManager = getPackageManager();
-
-        Intent cameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        List<ResolveInfo> cameraApps = packageManager.queryIntentActivities(cameraApp, PackageManager.MATCH_DEFAULT_ONLY);
-
-        return cameraApps.size() > 0;
-    }
-
-    // camera intent 전달
-    private void dispatchTakePictureIntent() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
         }
     }
+
+    // 권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+            Log.d("로그", "Permission: " + permissions[0] + " was " + grantResults[0]);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        switch (v.getId()) {
+            // 촬영
+            case R.id.btnCamera:
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, TAKE_PICTURE);
+                break;
+            case R.id.btnGallery:
+                intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                break;
+            case R.id.btnSave:
+                clickUpload();
+                imageUri = null;
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 사진 촬영 완료 후 응답
+        if(requestCode == TAKE_PICTURE) {
+            if(resultCode == RESULT_OK && data.hasExtra("data")) {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                if(bitmap != null) ivPill.setImageBitmap(bitmap);
+
+                String imageSaveUri = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "사진 저장", "찍은 사진이 저장되었습니다.");
+                imageUri = Uri.parse(imageSaveUri);
+                Log.d(TAG, "MainActivity - onActivityResult() called" + imageUri);
+            } // 갤러리에서 이미지 가져온 후의 응답
+        } else if(requestCode == GET_GALLERY_IMAGE){
+            if(resultCode == RESULT_OK && data.getData() != null) {
+                imageUri= data.getData();
+                Log.d(TAG, "MainActivity - onActivityResult() called" + imageUri);
+                Log.d(TAG, "MainActivity - onActivityResult() called" + getRealPathFromURI(imageUri));
+
+                Glide.with(this)
+                        .load(getRealPathFromURI(imageUri))
+                        .into(ivPill);
+
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+
+        String[] proj = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        cursor.moveToNext();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+        Uri uri = Uri.fromFile(new File(path));
+
+        cursor.close();
+        return path;
+    }
+
+    // 파이어베이스 업로드 함수
+    public void clickUpload() {
+
+        // 1. FirebaseStorage을 관리하는 객체 얻어오기
+        FirebaseStorage firebaseStorage= FirebaseStorage.getInstance();
+
+        // 2. 업로드할 파일의 node를 참조하는 객체
+        // 파일 명이 중복되지 않도록 날짜를 이용
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMddhhmmss");
+        String filename= sdf.format(new Date())+ ".png";
+        // 현재 시간으로 파일명 지정 20191023142634
+        // 원래 확장자는 파일의 실제 확장자를 얻어와서 사용해야함. 그러려면 이미지의 절대 주소를 구해야함.
+
+        StorageReference imgRef= firebaseStorage.getReference("uploads/"+filename);
+        // uploads라는 폴더가 없으면 자동 생성
+
+        // 참조 객체를 통해 이미지 파일 업로드
+        // imgRef.putFile(imgUri);
+        // 업로드 결과를 받고 싶다면 아래와 같이 UploadTask를 사용하면 된다.
+        UploadTask uploadTask =imgRef.putFile(imageUri);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this, "success upload", Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "MainActivity - onFailure() called");
+                    }
+                });
+    }
+
 }
+
