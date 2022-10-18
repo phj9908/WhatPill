88c534847a213eab8f50318a6e3e9d9f17926cee
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-18 20:36:30 +0900

    2022-10-18

--- app/src/main/java/com/example/whatpill/Information.java
+++ app/src/main/java/com/example/whatpill/Information.java
@@ -1,25 +1,36 @@
 package com.example.whatpill;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class Information extends AppCompatActivity {
 
-    public String name;
-
-    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mConditionRef = mDatabase.child("pill").child("name");
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     Button btn1;
 
@@ -29,26 +40,21 @@ public class Information extends AppCompatActivity {
         setContentView(R.layout.activity_information);
 
         btn1 = findViewById(R.id.btn1);
-    }
 
-    protected void onStart() {
-        super.onStart();
-
-        mConditionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.getValue(String.class) != null) {
-                    Object name = snapshot.getValue(String.class);
-                    btn1.setText(name.toString());
-                } else {
-                    Toast.makeText(getApplicationContext(), "데이터 없음", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        db.collection("aa")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 데이터를 가져오는 작업이 잘 동작했을 떄
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                btn1.setText(document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            btn1.setText("Error => " + task.getException());
+                        }
+                    }
+                });
     }
 }
