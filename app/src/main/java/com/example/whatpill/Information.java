e0568c7615cebebd2e425ecc45009cff98138365
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-19 19:05:46 +0900

    ver2 + scroll

--- app/src/main/java/com/example/whatpill/Information.java
+++ app/src/main/java/com/example/whatpill/Information.java
@@ -1,15 +1,35 @@
 package com.example.whatpill;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Information extends AppCompatActivity {
 
     Button btnExplain, btnPerson, btnPill, btnFood, btnGood;
+    ImageView ivPill;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +41,40 @@ public class Information extends AppCompatActivity {
         btnPill = findViewById(R.id.btnPill);
         btnFood = findViewById(R.id.btnFood);
         btnGood = findViewById(R.id.btnGood);
+        ivPill = findViewById(R.id.ivPill);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference pills = db.collection("jeong99").document("pills").collection("bearse");
+
 
         btnExplain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Worning.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Person.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Pill.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Food.class);
                 startActivity(intent);
             }
         });
