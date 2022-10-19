5d281cac1dd5e40a35603ce39c6b0350bda5ea7e
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-19 20:35:36 +0900

    ver4-button 개수

--- app/src/main/java/com/example/whatpill/Name.java
+++ app/src/main/java/com/example/whatpill/Name.java
@@ -18,23 +18,48 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+
 public class Name extends AppCompatActivity {
 
-    Button name1, name2, name3;
+    String pill [] = new String[3];
+    Button[] nameBtn = new Button[5];
+    Integer [] nameBtnID = {R.id.name1, R.id.name2, R.id.name3, R.id.name4, R.id.name5};
+
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
 
-        name1 = findViewById(R.id.name1);
-        name2 = findViewById(R.id.name2);
-        name3 = findViewById(R.id.name3);
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference pills = db.collection("jeong99").document("pills").collection("bearse");
 
-        pills.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        for (i=0; i<pill.length; i++) {
+            nameBtn[i] = findViewById(nameBtnID[i]);
+        }
+
+        for (i=0; i<pill.length; i++){
+            final int index;
+            index = i;
+
+            nameBtn[index].setVisibility(View.VISIBLE);
+
+            nameBtn[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Information.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+
+        /*pills.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
@@ -70,6 +95,6 @@ public class Name extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Information.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 }
