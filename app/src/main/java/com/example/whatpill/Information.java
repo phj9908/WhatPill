a147b92d67bba03f0fb9d776a84ea39fb05df6a3
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-19 15:07:46 +0900

    ver1

--- app/src/main/java/com/example/whatpill/Information.java
+++ app/src/main/java/com/example/whatpill/Information.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -26,13 +27,9 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.HashMap;
 import java.util.Map;
 
-
-
 public class Information extends AppCompatActivity {
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    Button btn1;
+    Button btn1, btn2, btn3, btn4, btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +37,33 @@ public class Information extends AppCompatActivity {
         setContentView(R.layout.activity_information);
 
         btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
 
-        db.collection("aa")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        // 데이터를 가져오는 작업이 잘 동작했을 떄
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                btn1.setText(document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            btn1.setText("Error => " + task.getException());
-                        }
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference pills = db.collection("jeong99").document("pills").collection("bearse");
+
+        pills.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String noperson = document.getString("noperson");
+                        String nopill = document.getString("nopill");
+                        String nofood = document.getString("nofood");
+                        btn1.setText(document.getId());
+                        btn2.setText(noperson);
+                        btn3.setText(nopill);
+                        btn4.setText(nofood);
+
                     }
-                });
+                } else{
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+
+            }
+        });
     }
 }
