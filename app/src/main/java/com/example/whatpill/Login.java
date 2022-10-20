78b159dc68d8d59e64170bedf8e08d34697e3956
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-20 21:08:42 +0900

    ver6 + login

--- app/src/main/java/com/example/whatpill/Login.java
+++ app/src/main/java/com/example/whatpill/Login.java
@@ -2,13 +2,28 @@ package com.example.whatpill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
