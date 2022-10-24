d8ad29d53ffdaef1a15f912140d562eb8867d110
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-10-24 16:49:38 +0900

    2020-10-24

--- app/src/main/java/com/example/whatpill/information/Information.java
+++ app/src/main/java/com/example/whatpill/information/Information.java
@@ -4,19 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.whatpill.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Information extends AppCompatActivity {
 
@@ -92,5 +97,30 @@ public class Information extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 약별로 사진 넣어줌
+        db.collection(pillName)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 데이터를 가져오는 작업이 잘 동작했을 떄
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String a = document.getString("image");
+
+                                FirebaseStorage storage = FirebaseStorage.getInstance("gs://what-pill.appspot.com");
+                                StorageReference storageReference = storage.getReference();
+                                StorageReference pathReference = storageReference.child(a);
+                                pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Glide.with(getApplicationContext()).load(uri).into(ivPill);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
     }
 }
